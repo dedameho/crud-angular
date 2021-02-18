231468a4485026a2462bd7999da336ed882603d1
dedameho <dedameho13@gmail.com>
2021-02-18 16:32:44 -0500

    jue feb 18, 16:32

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,8 +1,8 @@
+import { Toast } from 'materialize-css';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from './product.service';
 import { ActivatedRoute } from '@angular/router';
-import { RESOURCE_CACHE_PROVIDER } from '@angular/platform-browser-dynamic';
 
 @Component({
   selector: 'app-product',
@@ -13,34 +13,73 @@ export class ProductComponent implements OnInit {
   private idProduct:string;
   public productEdit:FormGroup;
   public nuevaFoto:File;
-  public producto:[];
+  public fotoProducto:string;
   constructor(private product:ProductService,private route:ActivatedRoute,private form:FormBuilder) {
     route.params.subscribe(params=>{
       const productId = params.id;
       this.idProduct=productId;
       this.product.getProduct(this.idProduct).subscribe(res=>{
-        this.producto=res;
-        res.forEach(element=>{
-          console.log(element.nombre);
+        this.fotoProducto=`http://localhost:3050/${res[0].imagen}`;
+        this.productEdit.setValue({
+          codigo:`${res[0].codigo}`,
+          referencia:`${res[0].referencia}`,
+          nombre:`${res[0].nombre}`,
+          foto:'',
+          ubicacion:`${res[0].ubicacion}`,
+          sede_bodega:`${res[0].sede_bodega}`,
+          descripcion:`${res[0].descripcion}`
         })
-        console.log(this.producto)
       })
     })
   }
 
   ngOnInit(){
     this.productEdit=this.form.group({
-      codigo:new FormControl('',Validators.required),
-      referencia:new FormControl('',Validators.required),
-      nombre:new FormControl('',Validators.required),
-      foto:new FormControl('',Validators.required),
-      ubicacion:new FormControl('',Validators.required),
-      sede_bodega:new FormControl('',Validators.required),
-      descripcion:new FormControl('',Validators.required)
+      codigo:new FormControl(' ',Validators.required),
+      referencia:new FormControl(' ',Validators.required),
+      nombre:new FormControl(' ',Validators.required),
+      foto:new FormControl(''),
+      ubicacion:new FormControl(' ',Validators.required),
+      sede_bodega:new FormControl(' ',Validators.required),
+      descripcion:new FormControl(' ',Validators.required)
     })
   }
   seleccionarFoto(file:File){
     this.nuevaFoto=file[0];
+    console.log(this.productEdit.value.foto);
+  }
+  updateProduct(){
+    var {codigo,referencia,nombre,ubicacion,sede_bodega,descripcion} = this.productEdit.value;
+    if(this.nuevaFoto){
+      this.product.actualizarProducto(this.idProduct,codigo,referencia,nombre,ubicacion,sede_bodega,descripcion,this.nuevaFoto).subscribe((res)=>{
+        if(res.status=='ok'){
+          new Toast({
+            html:'Producto Actualizado',
+            classes:'green'
+          })
+        }else{
+          new Toast({
+            html:`${res.message}`,
+            classes:'red'
+          })
+        }
+      })
+    }else{
+      this.product.actualizarProducto(this.idProduct,codigo,referencia,nombre,ubicacion,sede_bodega,descripcion).subscribe((res)=>{
+        if(res.status=='ok'){
+          new Toast({
+            html:'Producto Actualizado',
+            classes:'green'
+          })
+        }else{
+          new Toast({
+            html:`${res.message}`,
+            classes:'red'
+          })
+        }
+      })
+    }
+
   }
 
 }
--- src/app/product/product.service.ts
+++ src/app/product/product.service.ts
@@ -6,9 +6,23 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ProductService {
-  private url:string='http://localhost:3050/getproduct'
+  private url:string='http://localhost:3050/product'
   constructor(private http:HttpClient) { }
   getProduct(id:string):Observable<any>{
     return this.http.post(this.url,{id});
+  }
+  actualizarProducto(idproduct:string,codigo:string,referencia:string,nombre:string,ubicacion:string,sede_bodega:string,descripcion:string,foto?:File):Observable<any>{
+    const formData = new FormData();
+    formData.append('idproducto',idproduct);
+    formData.append('nombre',nombre);
+    formData.append('codigo',codigo);
+    formData.append('referencia',referencia);
+    formData.append('ubicacion',ubicacion);
+    formData.append('sede_bodega',sede_bodega);
+    formData.append('descripcion',descripcion);
+    if(foto){
+      formData.append('foto',foto);
+    }
+    return this.http.put(this.url,formData);
   }
 }
